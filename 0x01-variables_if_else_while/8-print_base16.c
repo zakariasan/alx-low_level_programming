09f256ecb7f2aa91dee00588ca4b1234785eea0d
zakariasan <elitziko@gmail.com>
2023-07-14 05:11:56 -0700

    ALL in hex and rev alpha

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,12 @@ int main(void)
 {
 	char c;
 
-	c = 'z';
-	while (c >= 'a')
-		putchar(c--);
+	c = '0';
+	while (c <= '9')
+		putchar(c++);
+	c = 'a'
+	while (c <= 'f')
+		putchar(c++);
 	putchar('\n');
 	return (0);
 }
