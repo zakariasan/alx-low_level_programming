e1a59d07b4d3e2662aa80b2741132d67c3d7385a
zakariasan <elitziko@gmail.com>
2023-07-14 04:58:01 -0700

    Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: print Numbers 
+ * Return: main return 0 all safe
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+		putchar(c++);
+	putchar('\n');
+	return (0);
+}
