891f865718ae2ae4e6531036cfd8a077e6977bb2
zakariasan <elitziko@gmail.com>
2023-07-14 06:10:32 -0700

    Combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,26 +4,32 @@
 
 /**
  * main - Entry Point
- * Description: print combination 2 digit
+ * Description: Combine 3 digit -_-
  * Return: main return 0 all safe
  */
 int main(void)
 {
 	int c;
 	int j;
+	int k;
 
 	c = '0';
 	while (c <= '9')
 	{
-		j = c + 1;
-		while (j <= '9')
+		j = c;
+		while (++j <= '9')
 		{
-			putchar(c);
-			putchar(j);
-			if (j++ != '9' || c != '8')
+			k = j;
+			while (++k <= '9')
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(c);
+				putchar(j);
+				putchar(k);
+				if (j != '8' || c != '7' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 		c++;
