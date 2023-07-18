5cc40fa9005618031da8032ee26e876579803c84
zakariasan <elitziko@gmail.com>
2023-07-18 13:38:19 -0700

    fiboo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	int n1;
-	int n2;
-	int fibo;
+	unsigned int n1;
+	unsigned int n2;
+	unsigned int fibo;
 	int i;
 
 	n1 = 1;
 	n2 = 2;
 	i = 0;
-	printf("%d, %d, ",n1,n2);
+	printf("%u, %u, ",n1,n2);
 	while (i < 50)
 	{
 		fibo = n1 + n2;
-		printf("%d",fibo);
+		printf("%u",fibo);
 		n1 = n2;
 		n2 = fibo;
 		if (i++ != 46)
