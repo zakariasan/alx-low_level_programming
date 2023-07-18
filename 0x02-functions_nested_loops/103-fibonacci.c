4ba51c139c925f7117a1003ca7127078321bb682
zakariasan <elitziko@gmail.com>
2023-07-18 13:57:57 -0700

    fibo second

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print fibonacci
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	long int n1;
+	long int n2;
+	long int fibo;
+	long int sum;
+	int i;
+
+	n1 = 1;
+	n2 = 2;
+	i = 0;
+	while (i < 40)
+	{
+		if(n1 < 4000000 && (n1 % 2) == 0 )
+			sum += n1;
+		fibo = n1 + n2;
+		n1 = n2;
+		n2 = fibo;
+		i++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
