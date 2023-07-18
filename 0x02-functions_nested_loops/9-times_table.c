8c7deee002a703c7b1a25c9f3fc839c5b1497aff
zakariasan <elitziko@gmail.com>
2023-07-18 07:40:10 -0700

    Tiems table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,17 +15,19 @@ void times_table(void)
 	{
 		col = -1;
 		tmp = 0;
-		while (++col < 9)
+		while (++col <= 9)
 		{
 			if (tmp >= 10)
 			{
 				_putchar((tmp / 10) + '0');
 				_putchar((tmp % 10) + '0');
 			}
-			_putchar(tmp + '0');
+			else
+				_putchar(tmp + '0');
 			if (col != 8)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 			}
 			else
