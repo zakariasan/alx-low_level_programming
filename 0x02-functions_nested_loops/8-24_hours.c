10ac724ee58633e825a9d36de78b80858b0bddb8
zakariasan <elitziko@gmail.com>
2023-07-18 07:19:00 -0700

    Enjoy song

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int min;
+	int sec;
+
+	min = -1;
+	while (++min < 24)
+	{
+		sec = -1;
+		while (++sec < 60)
+		{
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar(':');
+			_putchar(sec / 10 + '0');
+			_putchar(sec % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
