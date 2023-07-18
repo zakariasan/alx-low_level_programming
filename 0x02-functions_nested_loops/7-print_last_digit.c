9afbc17d670ff9666155d80185f3093a32bc8175
zakariasan <elitziko@gmail.com>
2023-07-18 06:41:07 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,6 @@
  */
 int print_last_digit(int n)
 {
-	if (n >= 10)
-	{
-		_putchar(n % 10 + '0');
-		return (n % 10);
-	}
-	_putchar(n % 10 + '0');
-	return (n);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
