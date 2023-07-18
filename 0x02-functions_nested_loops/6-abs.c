67b996d36ec592fc6f9b9bad4d3c9b03e46f490a
zakariasan <elitziko@gmail.com>
2023-07-18 06:29:02 -0700

    Abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - abs value
+ * @n: int value
+ *
+ * Description: abs value
+ * Return: On success with abs(n)
+ */
+int _abs(int n)
+{
+	return ((n >= 0) ? n : -n);
+}
