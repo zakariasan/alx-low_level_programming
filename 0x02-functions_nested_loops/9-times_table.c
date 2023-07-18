c878a7b169d90777104ef6f39b2f6958f32d157c
zakariasan <elitziko@gmail.com>
2023-07-18 09:58:01 -0700

    Print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 		{
 			if (tmp >= 10)
 				_putchar((tmp / 10) + '0');
-			else if (tmp > 0)
+			else if (col > 0)
 				_putchar(' ');
 			_putchar((tmp % 10) + 48);
 			if (col != 9)
