860ae162158c0cb9b6562e13ae27da538bbd0345
zakariasan <elitziko@gmail.com>
2023-07-18 05:56:19 -0700

    Lower Case Function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print x10 alpha
+ * @c: The Char to test
+ *
+ * Return: On success 1.Upper Case Return 0
+ */
+void _islower(char c)
+{
+	if(c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
