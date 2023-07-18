828ea1e08378c2f03e9d4908efd37889aa518654
zakariasan <elitziko@gmail.com>
2023-07-18 11:13:22 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,46 @@
 #include "main.h"
 
 /**
+ * _putnbr - Print nbr.
+ * @nbr:nbr to print.
+ */
+void _putnbr(int nbr)
+{
+	int i;
+
+	i = 10;
+	if (nbr < 0)
+	{
+		_putchar('-');
+		nbr = -nbr;
+	}
+	while (nbr / i)
+		i = i * 10;
+	i = i / 10;
+	while (i > 1)
+	{
+		_putchar((nbr / i) + '0');
+		nbr = nbr % i;
+		i = i / 10;
+	}
+	_putchar(nbr + '0');
+}
+
+/**
  * print_to_98 - Print nbr to 98.
- * @a: start from nbr.
+ * @n: start from nbr.
  */
 void print_to_98(int n)
 {
-	char tmp;
-
-	while (n <= 98)
+	while (n != 99)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			tmp = -n;
-		}
-		else
-			tmp = n;
-		if ((tmp / 10) != 0)
-		{	
-			_putchar(tmp / 10 + '0');
-			_putchar(tmp % 10 + '0');
-		}
-		else
-			_putchar(tmp + '0');
-		if (n != 98)
+		_putnbr(n);
+		if(n != 98)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-		n++;
+		(n > 98) ? n-- : n++;
 	}
 	_putchar('\n');
 }
