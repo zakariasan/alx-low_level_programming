95c0e9c6efaf434dd2079fadc282669d6af262da
zakariasan <elitziko@gmail.com>
2023-07-18 06:17:46 -0700

    Print Sign of nbr

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ * @c: int value
+ *
+ * Description: prints + or - or 0 the sign of a number.
+ * Return: On success 1.Upper Case Return 0
+ */
+int _isalpha(int n)
+{
+	if (n >= 0)
+	{
+		if (n == 0)
+		{
+			_putchar('0');
+			return (0);
+		}
+		_putchar('+');
+		return (1);
+	}
+	_putchar('-');
+	return (-1);
+}
