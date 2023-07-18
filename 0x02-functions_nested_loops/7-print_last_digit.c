20441591c29e2fa8730ae931d0b523954a1f519a
zakariasan <elitziko@gmail.com>
2023-07-18 06:32:04 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  Print Last digit.
+ * @n: int value
+ *
+ * Description: Print last digit of nbr.
+ * Return: On success last degit(n).
+ */
+int print_last_digit(int n)
+{
+	if(n >= 10)
+		return (n % 10);
+	return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int _islower(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 
 #endif
