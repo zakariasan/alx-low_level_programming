71ae860f91a7705497187225061dc6784beac629
zakariasan <elitziko@gmail.com>
2023-07-18 18:58:46 -0700

    fibo next

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,28 +7,32 @@
  * Return: On success 0.
  */
 #include <stdio.h>
+int main(void)
+{
+	long int i, n1, n2, j1, j2, k1, k2;
 
-int main(void) {
-    unsigned long int i, j, k;
-
-    j = 1;
-    k = 2;
-
-    printf("%lu", j);
-
-    for (i = 1; i < 91; i++) {
-        printf(", %lu", k);
-        k = k + j;
-        j = k - j;
-    }
-
-    for (i = 92; i < 99; ++i) {
-        printf(", %lu%09lu", k / 1000000000, k % 1000000000);
-        k = k + j;
-        j = k - j;
-    }
-
-    printf("\n");
-
-    return 0;
+	n1 = 1;
+	n2 = 2;
+	printf("%ld", n1);
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %ld", n2);
+		n2 = n2 + n1;
+		n1 = n2 - n1;
+	}
+	j1 = n1 / 1000000000;
+	j2 = n1 % 1000000000;
+	k1 = n2 / 1000000000;
+	k2 = n2 % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+	printf("\n");
+	return (0);
 }
