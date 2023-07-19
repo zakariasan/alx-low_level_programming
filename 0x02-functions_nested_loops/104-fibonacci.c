925ca16300ba95189c6536d80af17508479436ef
zakariasan <elitziko@gmail.com>
2023-07-18 17:35:23 -0700

    last Up fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print fibonacci
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	long int n1;
+	long int n2;
+	long int fibo;
+	int i;
+
+	n1 = 1;
+	n2 = 2;
+	i = 0;
+	sum = 0;
+	while (i < 98)
+	{
+		if (i < 97)
+			printf("%ld", n1);
+		fibo = n1 + n2;
+		n1 = n2;
+		n2 = fibo;
+		i++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
