5f132c2198cf0ea7e305d62ee0a528dd33df8fd5
zakariasan <elitziko@gmail.com>
2023-07-20 06:57:31 -0700

    I feel like I am diagonally parked

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal - Print a diagnonal line.
+* @n: length of line.
+* Description : Draw diagonal line.
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int sp;
+
+	i = -1;
+	while (++i < n)
+	{
+		sp = 0;
+		while (sp++ <= i)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n == 0)
+		_putchar('\n');
+}
