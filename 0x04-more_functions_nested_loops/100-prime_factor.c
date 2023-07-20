468906a3c58de2b41cd5e550ab8e31ad4ee4ca96
zakariasan <elitziko@gmail.com>
2023-07-20 08:55:05 -0700

    Prime nbr

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+* main - Fizz Buzz.
+* Description : Write a program that prints the numbers from 1 to 100.
+* Return: Always 0 (Sucess).
+*/
+int main(void)
+{
+	int i;
+
+	unsigned long int nbr = 612852475143;
+	printf("%uld\n", 1);
+	return (0);
+}
