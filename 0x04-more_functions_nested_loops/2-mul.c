c19679c45268c75ca19efdee170946a2e08a8db9
zakariasan <elitziko@gmail.com>
2023-07-20 06:03:36 -0700

    Multi nbr 1 nbr2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - mult tow digit.
+* @a: int value
+* @b: int value
+*
+* Description : mult a and b.
+* Return: return a mul b.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
