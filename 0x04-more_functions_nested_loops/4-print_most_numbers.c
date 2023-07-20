9a0a0efbd42ed427f38f64b45f5c3cde3a66ab6c
zakariasan <elitziko@gmail.com>
2023-07-20 06:35:08 -0700

    Most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
 * (not 2 and 4), from 0 to 9 \n.
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
 	c = '0';
 	while (c <= '9')
-		(c == '2' || c == '4') ? _putchar(c++) : c++;
+		(c == '2' || c == '4') ? c++ : _putchar(c++);
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,29 @@
 #include "main.h"
+/**
+ * _putnbr - Print nbr.
+ * @nbr:nbr to print.
+ */
+void _putnbr(int nbr)
+{
+	int i;
+
+	i = 10;
+	if (nbr < 0)
+	{
+		_putchar('-');
+		nbr = -nbr;
+	}
+	while (nbr / i)
+		i = i * 10;
+	i = i / 10;
+	while (i > 1)
+	{
+		_putchar((nbr / i) + '0');
+		nbr = nbr % i;
+		i = i / 10;
+	}
+	_putchar(nbr + '0');
+}
 
 /**
 * print_most_numbers - Print nbrs.
