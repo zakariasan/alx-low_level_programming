c964d9189b4844d55166cbf6c3e9d68f4e54c9db
zakariasan <elitziko@gmail.com>
2023-07-24 12:14:18 -0700

    101Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,51 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define MAX_PASSWORD_LENGTH 12
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int password[55];
+	int i;
+	int sum;
 
-int random_range(int min, int max) {
-    return rand() % (max - min + 1) + min;
-}
-
-char random_lowercase_letter() {
-    return 'a' + random_range(0, 25);
-}
-
-char random_uppercase_letter() {
-    return 'A' + random_range(0, 25);
-}
-
-char random_digit() {
-    return '0' + random_range(0, 9);
-}
-
-void generate_password(char* password) {
-    int length = random_range(6, MAX_PASSWORD_LENGTH);
-    int i;
-    int choice;
-
-    for (i = 0; i < length; i++) {
-	    choice = random_range(0, 2);
-        if (choice == 0) {
-            password[i] = random_lowercase_letter();
-        } else if (choice == 1) {
-            password[i] = random_uppercase_letter();
-        } else {
-            password[i] = random_digit();
-        }
-    }
-    password[i] = '\0'; 
-}
-
-int main() {
-
-    char password[MAX_PASSWORD_LENGTH + 1];
-    srand(time(NULL)); 
-    generate_password(password);
-
-    printf("%s\n", password);
-
-    return 0;
+	sum = 0;
+	i = 0;	
+	srand(time(NULL));
+	while (i <= 55)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i++] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			putchar(2772 - sum + '0');
+			return (0);
+		}
+	}
+	return (0);
 }
