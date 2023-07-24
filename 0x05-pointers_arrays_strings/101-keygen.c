60418e81df082c14fdad7aa74ec3a72828f3d34e
zakariasan <elitziko@gmail.com>
2023-07-24 10:44:10 -0700

    Keygen true

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,35 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+
+ * checksum - Calculate the sum of every character of a string
+
  *
- * Return: Always 0 (Success)
- */
-int main(int ac, char **argv)
+
+ * @p: A pointer to the first character of a string
+
+ * Return: The sum of every character in @p
+
+*/
+
+unsigned long int checksum(char *p)
+
 {
+
 	unsigned long int c;
 
+
+
 	c = 0;
-	if (ac != 2)
+
+	while (*p)
+
 	{
-		printf("Usage: %s password\n", argv[0]);
-		return (1);
+
+		c += *p;
+
+		p++;
+
 	}
-	while (*argv[1])
-	{
-		c += *argv[1];
-		argv[1]++;
-	}
-	if (c != 2772)
-	{
-		printf("Wrong password\n");
-		return (1);
-	}
-	printf("Tada! Congrats\n");
-	return (0);
-					
+
+	return (c);
+
 }
 
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * @ac: Arguments counter
+
+ * @av: Arguments array
+
+ * Return: 1 on error, 0 otherwise
+
+*/
+
+int main(int ac, char **av)
+
+{
+
+	unsigned long int c;
+
+
+
+	if (ac != 2)
+
+	{
+
+		printf("Usage: %s password\n", av[0]);
+
+		return (1);
+
+	}
+
+	c = checksum(av[1]);
+
+	/* printf("%lu\n", c); */
+
+	/* "Talk is cheap. Show me the code." */
+
+	if (c != 2772)
+
+	{
+
+		printf("Wrong password\n");
+
+		return (1);
+
+	}
+
+	printf("Tada! Congrats\n");
+
+	return (0);
+
+}
