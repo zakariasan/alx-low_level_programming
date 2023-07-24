3756838eb05fa811e924efd478b50d86772c044b
zakariasan <elitziko@gmail.com>
2023-07-24 11:47:05 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,18 +3,15 @@
 #include <time.h>
 
 int main() {
-  // Seed the random number generator with the current time.
-  time_t t;
-  srand(time(&t));
+	srand(time(0));
 
-  // Generate a random password of length 10.
-  char password[10];
-  for (int i = 0; i < 10; i++) {
-    password[i] = rand() % 95 + 32; // Generate a random character between ' ' and '~'.
-  }
+	char password[10];
+	for (int i = 0; i < 10; i++)
+	{
+		password[i] = rand() % 95 + 32;
+	}
 
-  // Print the password.
-  printf("%s\n", password);
+	printf("%s\n", password);
 
-  return 0;
+	return 0;
 }
