c21eb83538ae2bca36869c4e5be18ef3a4788412
zakariasan <elitziko@gmail.com>
2023-07-24 11:49:23 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,10 +3,13 @@
 #include <time.h>
 
 int main() {
-	srand(time(0));
 
 	char password[10];
-	for (int i = 0; i < 10; i++)
+	int i;
+
+	srand(time(0));
+
+	for (i = 0; i < 10; i++)
 	{
 		password[i] = rand() % 95 + 32;
 	}
