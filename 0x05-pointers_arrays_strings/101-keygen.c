3600a54e6365031e7de927b1936537f1e84903d1
zakariasan <elitziko@gmail.com>
2023-07-24 11:18:24 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-#define PASSWORD_LENGTH 15
-#define CORRECT_PASSWORD "Tada! Congrats"
 
-void generate_password(char *password) {
-  char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{[]}|;:'\",./?";
-  int i;
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
 
-  srand(time(NULL));
+	sum = 0;	
 
-  for (i = 0; i < PASSWORD_LENGTH; i++) {
-    password[i] = characters[rand() % (sizeof(characters) - 1)];
-  }
-}
+	srand(time(NULL));
 
-int check_password(char *password) {
-  return strcmp(password, CORRECT_PASSWORD) == 0;
-}
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			putchar(n + '0');
+			break;
+		}
+	}
 
-int main() {
-  char password[PASSWORD_LENGTH];
-
-  generate_password(password);
-
-  if (check_password(password)) {
-    printf("Tada! Congrats\n");
-  } else {
-    printf("Wrong password\n");
-  }
-
-  return 0;
+	return (0);
 }
