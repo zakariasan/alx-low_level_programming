12517b5ed847873227addd8ad612978d04c180fa
zakariasan <elitziko@gmail.com>
2023-07-24 12:01:02 -0700

    KeyGen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,33 +5,29 @@
 
 #define MAX_PASSWORD_LENGTH 12
 
-// Function to generate a random number between min and max (inclusive)
 int random_range(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-// Function to generate a random lowercase letter
 char random_lowercase_letter() {
     return 'a' + random_range(0, 25);
 }
 
-// Function to generate a random uppercase letter
 char random_uppercase_letter() {
     return 'A' + random_range(0, 25);
 }
 
-// Function to generate a random digit
 char random_digit() {
     return '0' + random_range(0, 9);
 }
 
-// Function to generate a random password
 void generate_password(char* password) {
-    int length = random_range(6, MAX_PASSWORD_LENGTH); // Choose a random length between 6 and MAX_PASSWORD_LENGTH
+    int length = random_range(6, MAX_PASSWORD_LENGTH);
     int i;
+    int choice;
 
     for (i = 0; i < length; i++) {
-        int choice = random_range(0, 2);
+	    choice = random_range(0, 2);
         if (choice == 0) {
             password[i] = random_lowercase_letter();
         } else if (choice == 1) {
@@ -40,11 +36,11 @@ void generate_password(char* password) {
             password[i] = random_digit();
         }
     }
-    password[i] = '\0'; // Null-terminate the password
+    password[i] = '\0'; 
 }
 
 int main() {
-    srand(time(NULL)); // Seed the random number generator with the current time
+    srand(time(NULL)); 
 
     char password[MAX_PASSWORD_LENGTH + 1];
     generate_password(password);
