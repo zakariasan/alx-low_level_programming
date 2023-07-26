235128cc109fd695bd440f7f9c31f863174df375
zakariasan <elitziko@gmail.com>
2023-07-26 09:36:47 -0700

    Capitalize String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,11 +13,13 @@ char *cap_string(char *str)
 	int i;
 
 	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
 	while (str[i])
 	{
 		if (str[i] <= ' ' || str[i] == ',' || str[i] == ';' ||
 				str[i] == '.' || str[i] == '!' ||
-				str[i] == '?' || str[i] == '\"' ||
+				str[i] == '?' || str[i] == '"' ||
 				str[i] == '(' || str[i] == ')' ||
 				str[i] == '{' || str[i] == '}')
 		{
