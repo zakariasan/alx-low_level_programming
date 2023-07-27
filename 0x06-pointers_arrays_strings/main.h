c949c50d9954e0ece8f3f1be177dc5c2fbaf76ae
zakariasan <elitziko@gmail.com>
2023-07-27 09:07:19 -0700

    It is the addition of strangeness

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 int _strcmp(char *s1, char *s2);
 
