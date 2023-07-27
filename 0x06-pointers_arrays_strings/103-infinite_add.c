202787ac95ef14f8f796899d6a16b63ac9625406
zakariasan <elitziko@gmail.com>
2023-07-27 08:44:38 -0700

    It is the addition of strangeness to beauty

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,81 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strrev - Reverse str.
+* @str: str.
+*
+* Description: function that reverses the
+* content of an array of str
+* Return: reversed str
+*/
+
+char *_strrev(char *str)
+{
+	int i;
+	int j;
+	int tmp;
+
+	j = strlen(str);
+	i = -1;
+	tmp = 0;
+	while (++i < --j)
+	{
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+	}
+	return (str);
+}
+
+/**
+* infinite_add -  adds two numbers.
+* @n1: str nbr1.
+* @n2: str2 nbr2.
+* @r: buffer.
+* @size_r: buffer size.
+*
+* Description: function that adds two numbers.
+* Return: value of adder
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+	int j;
+	int nbr1;
+	int nbr2;
+	int buffer;
+	int size_count;
+
+	i = strlen(n1);
+	j = strlen(n2);
+	buffer = 0;
+	size_count = 0;
+	if (--i >= size_r || --j  >= size_r)
+		return (0);
+	while (i >= 0 || j >= 0 || buffer == 1)
+	{
+		(i < 0) ? (nbr1 = 0) : (nbr1 = n1[i] - '0');
+		(j < 0) ? nbr2 = 0 : (nbr2 = n2[j] - '0');
+		if ((nbr1 + nbr2 + buffer) >= 10)
+		{
+			*(r + size_count) = (nbr1 + nbr2 + buffer) % 10 + '0';
+			buffer = 1;
+		}
+		else
+		{
+			*(r + size_count) = (nbr1 + nbr2 + buffer) + '0';
+			buffer = 0;
+		}
+		size_count++;
+		i--;
+		j--;
+		if (size_count == size_r)
+			return (0);	
+	}
+	*(r + size_count) = 0;
+	r = _strrev(r);
+	return (r);
+}
