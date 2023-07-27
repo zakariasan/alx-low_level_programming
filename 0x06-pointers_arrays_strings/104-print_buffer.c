5fbf9948f0bcc27000a4d3cca00812615a57ea6d
zakariasan <elitziko@gmail.com>
2023-07-27 11:10:57 -0700

    Noise is a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_buffer - Print Buffer.
+* @b: buffer.
+* @size: buffer size.
+*
+* Description: function that prints a buffer.
+*/
+
+void print_buffer(char *b, int size)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < size)
+	{
+		printf("%08x: ", i);
+		j = 0;
+		while (j < 10 && (i + j) < size)
+		{
+			printf("%02x", b[i + j]);
+			j++;
+			if (j % 2 == 0)
+				printf(" ");
+		}
+		while (j < 10)
+		{
+			printf("  ");
+			if (j++ % 2 == 0)
+				printf(" ");
+		}
+		j = 0;
+		while (j < 10 && (i + j) < size)
+		{
+			if (b[i + j] < 32)
+				printf(".");
+			else
+				printf("%c", b[i + j]);
+			j++;
+		}
+		printf("\n");
+		i += 10;
+	}
+}
