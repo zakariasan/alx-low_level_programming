069de2ffe9665d41d25abd9877a419ebd4aa9a71
zakariasan <elitziko@gmail.com>
2023-07-31 09:38:34 -0700

    Print diag sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagsums - Sum of the two diagonals.
+* @a: matrix tab.
+* @size: matrix size.
+*
+* Description :  function that prints the sum of the
+* two diagonals of a square matrix of integers.
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int diag1;
+	int diag2;
+
+	i = -1;
+	diag1 = 0;
+	diag2 = 0;
+	while (++i < size)
+	{
+		diag1 += a[(size * i) + i];
+		diag2 += a[(size * (i + 1)) - i - 1];
+	}
+	printf("%d, %d\n", diag1,diag2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 void _putchar(char c);
 
 #endif /* MAIN_H */
