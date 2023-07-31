67a69b3faf9c48aff9359c505797de26cf9b596d
zakariasan <elitziko@gmail.com>
2023-07-31 10:45:51 -0700

    ptr game play

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - ptr to char.
+* @s: string source
+* @to: dest str.
+*
+* Description :function that sets the value of a pointer to a char.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
