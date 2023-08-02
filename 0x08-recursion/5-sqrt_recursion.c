3819a41890d3d3f976eeb2baa0b4eb2b53735dac
zakariasan <elitziko@gmail.com>
2023-08-01 22:31:55 -0700

    Your subconscious Looking for

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _sqrt_recursion2 - square root of a nbr
+* @n: nbr
+*
+* Description : function that returns the natural square root of a number.
+* Return: sqrt(n) or 0
+*/
+
+int _sqrt_recursion2(int n, int nbr)
+{
+	if (nbr < 0)
+		return (-1);
+	if (n * n < nbr)
+		return (_sqrt_recursion2(n + 1, nbr));
+	return ((n * n) == nbr ? n : -1);
+}
+
+/**
+* _sqrt_recursion - call sqrt for recursion
+* @n: nbr
+*
+* Description : function that returns the natural square root of a number.
+* Return: sqrt(n) or 0
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursion2(0, n));
+}
