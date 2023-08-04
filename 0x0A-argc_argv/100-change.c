52f0d45a0762a5e618c8e080bd3dab5d5ded761c
zakariasan <elitziko@gmail.com>
2023-08-04 18:41:50 +0100

    Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,34 +14,27 @@ int main(int ac, char **av)
 {
 	int amount;
 	int cache[] = {25, 10, 5, 2, 1};
-	int back;
 	int i;
 	int tms;
 
 	if (ac == 2)
 	{
 		amount = atoi(av[1]);
-		back = 0;
 		i = 0;
 		tms = 0;
-		if (amount < 0)
+		while (amount != 0)
 		{
-			printf("0\n");
-			return (0);
-		}
-		while (back != amount)
-		{
-			back += cache[i];
-			tms++;
-			if (back > amount)
+			if (amount < 0)
 			{
-				back -= cache[i];
+				amount += cache[i];
 				if (i > 5)
 					i = 4;
 				else
 				 i++;
 				tms--;
 			}
+			amount -= cache[i];
+			tms++;
 		}
 		printf("%d\n", tms);
 	}
