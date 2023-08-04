a8e882c71bf08eb0ed3fd80ce2cfb7d0c8bb3aad
zakariasan <elitziko@gmail.com>
2023-08-04 17:31:45 +0100

    Minimal Number

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - adds positive numbers.
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: Sucess.
+ */
+
+int main(int ac, char **av)
+{
+	int amount;
+	int cache[5] = {25, 10, 5, 2, 1};
+	int back;
+	int i;
+	int tms;
+
+	if (ac == 2)
+	{
+		amount = atoi(av[1]);
+		back = 0;
+		i = 0;
+		tms = 0;
+		if (amount < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		while (back != amount)
+		{
+			if (back > amount)
+			{
+				back -= cache[i];
+				i++;
+				tms--;
+			}
+			if (i > 5)
+				i = 4;
+			back += cache[i];
+			tms++;
+		}
+		printf("%d\n", tms);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
