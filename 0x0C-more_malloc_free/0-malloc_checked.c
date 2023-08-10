725cd56c2b3d1dcc06ce91e4f7d8471a29e6d42b
zakariasan <elitziko@gmail.com>
2023-08-10 10:29:48 +0100

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size of allocated memo.
+ *
+ * Description: function that allocates memory using malloc.
+ * Return: 98 or ptr of memo
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr= malloc(b);
+	if (!ptr)
+		return ((void *)98);
+	return (ptr);
+}
+
