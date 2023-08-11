1ae053d1afc485b555dbbaa2f777601bcc655bbd
zakariasan <elitziko@gmail.com>
2023-08-11 02:07:48 +0100

    We must accept finite

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,22 +12,68 @@
  * Description: func to mult digit by digit
  * Return: On success a nbr. else NULL
  */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str)
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+/**
+ * _calloc - calloc func
+ * @nmemb: nbr of elements.
+ * @size: sizeof Element.
+ *
+ * Description: function that allocates memory for an array
+ * using malloc.
+ * Return: memo set to zero with nmemb elements or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *caloc;
+	unsigned int i;
+
+	if (!nmemb || !size)
+		return (NULL);
+	caloc = malloc(size * nmemb);
+	if (!caloc)
+		return (NULL);
+	i = -1;
+	while (++i < (nmemb * size))
+		((char *)caloc)[i] = 0;
+	return (caloc);
+}
+/**
+ * multy - mult digits
+ * @num1: str argument nb1
+ * @num2: str argument nb2
+ *
+ * Description: func to mult digit by digit
+ * Return: On success a nbr. else NULL
+ */
 char *multy(char *num1, char *num2)
 {
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
 	int resultLen;
 	int *result;
 	int idx = 0;
 	int mul;
 	int sum;
 	char *resultStr;
-
+	int i;
+	int j;
 	resultLen = len1 + len2;
-	result = (int *)calloc(resultLen, sizeof(int));
-	for (int i = len1 - 1; i >= 0; i--)
+	result = (int *)_calloc(resultLen, sizeof(int));
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		for (int j = len2 - 1; j >= 0; j--)
+		for (j = len2 - 1; j >= 0; j--)
 		{
 			mul = (num1[i] - '0') * (num2[j] - '0');
 			sum = result[i + j + 1] + mul;
@@ -38,7 +84,7 @@ char *multy(char *num1, char *num2)
 	resultStr = (char *) malloc((resultLen + 1) * sizeof(char));
 	if (!resultStr)
 		return (NULL);
-	for (int i = 0; i < resultLen; i++)
+	for (i = 0; i < resultLen; i++)
 	{
 		if (idx == 0 && result[i] == 0)
 			continue;
@@ -77,7 +123,6 @@ int _isalpha(char *str)
  */
 int main(int ac, char **av)
 {
-	unsigned long  mul;
 	char *res;
 
 	if (ac != 3 || _isalpha(av[1]) || _isalpha(av[2]))
