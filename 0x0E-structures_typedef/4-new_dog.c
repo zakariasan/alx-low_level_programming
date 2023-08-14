931a4ec4ba4d8a9dcfce5aeb8c24f90065f37d82
zakariasan <elitziko@gmail.com>
2023-08-14 12:02:02 +0100

    got by dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,16 +52,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
 
-	n_dog = malloc(sizeof(dog_t));
+	n_dog = malloc(sizeof(*n_dog));
 	if (!n_dog)
 		return (NULL);
-	n_dog->name = malloc(sizeof(n_dog->name) * (_strlen(name)));
+	n_dog->name = malloc(sizeof(char) * (_strlen(name)));
 	if (!n_dog->name)
 	{
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->owner = malloc(sizeof(n_dog->owner) * (_strlen(owner)));
+	n_dog->owner = malloc(sizeof(char) * (_strlen(owner)));
 	if (!n_dog->owner)
 	{
 		free(n_dog->name);
