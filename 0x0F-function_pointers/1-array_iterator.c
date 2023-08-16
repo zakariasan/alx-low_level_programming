8cce0de9d941fb0f13ef8c76c18413d2e1ad0f01
zakariasan <elitziko@gmail.com>
2023-08-16 13:01:07 +0100

    If you spend too much time thinking about a thing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	i = 0;
-	while (i < size)
-		(*action)(array[i++]);
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+			(*action)(array[i++]);
+	}
 }
