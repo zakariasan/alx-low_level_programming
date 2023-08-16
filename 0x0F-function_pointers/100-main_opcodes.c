786b2ffdc31934dba8f037543ae60b19173694c1
zakariasan <elitziko@gmail.com>
2023-08-16 15:33:32 +0100

    Most hackers are young tell me why

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Operation code implimentation.
+ * @ac: nbr of argument
+ * @av: vector of args
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int value;
+
+	value = 0;
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(av[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (value < atoi(av[1]))
+	{
+		printf("%02x", ((unsigned char *)main)[value++]);
+		if (value < atoi(av[1]) - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
