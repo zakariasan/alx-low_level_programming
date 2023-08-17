1af223c557f6746a5ea0a4e8e06570b6ba7e74bc
zakariasan <elitziko@gmail.com>
2023-08-17 11:03:43 +0100

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
 /**
  * sum_them_all - sum of params.
  * @n: nbr of parms.
- * @...: var args
  *
  * Description:function that returns the sum of all its parameters.
  * Return: sum of 0.
@@ -13,14 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum;
-	va_list ap_arg;
+	va_list ap_args;
 
 	sum = 0;
 	if (n == 0)
-		return (sum);
-	va_start(ap_arg, n);
+		return (0);
+	va_start(ap_args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap_arg, int);
-	va_end(ap_arg);
+		sum += va_arg(ap_args, int);
+	va_end(ap_args);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,19 +2,6 @@
 #define _VARIADIC_FUNC_H_
 
 #include <stdarg.h>
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct ops
-{
-	char *ops;
-	void (*f)(void *);
-}		ops_t;
-
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
