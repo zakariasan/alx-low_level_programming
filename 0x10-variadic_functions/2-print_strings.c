0b5b6812eae9d48d8121575663be652278ce6d11
zakariasan <elitziko@gmail.com>
2023-08-17 11:43:56 +0100

    woman's constant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,17 +14,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap_arg;
 	char *str;
 
-	if (n > 0 && separator)
+	if (n > 0)
 	{
 		va_start(ap_arg, n);
 		for (i = 0; i < n; i++)
 		{
 			str = va_arg(ap_arg, char *);
 			(str) ? printf("%s", str) : printf("(nil)");
-			if (i < n - 1)
+			if (i < n - 1 && separator)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(ap_arg);
 	}
+	printf("\n");
 }
