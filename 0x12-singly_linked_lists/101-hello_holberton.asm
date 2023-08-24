e693676b13efe83318cb99b9bf9a3f8a106bd15a
zakariasan <elitziko@gmail.com>
2023-08-24 13:16:55 +0100

    hello helberton with asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
 global main
 section .data
-	my_str db "Hello, Holberton\n", 0x0a
+	my_str db "Hello, Holberton", 0x0a
 	len equ $ - my_str
 section .text
 main:
-	mov eax,4
-	mov ebx,1
-	mov ecx, my_str
-	mov edx, len
-	int 0x80
-	mov eax, 1
-	mov ebx, 0
-	int 0x80
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [my_str]
+	mov rdx, len
+	syscall
+	mov rax,60
+	xor rdi, rdi
+	syscall
