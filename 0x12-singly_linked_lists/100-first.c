5c47551570159d010105699efd6f0f8ae3cfbdb9
zakariasan <elitziko@gmail.com>
2023-08-24 12:44:03 +0100

    fix print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,10 +6,10 @@
  *
  * Description:function arrive first before main.
  */
-void hare() __attribute__((constructor));
+void hare(void) __attribute__((constructor));
 
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("my back!\n");
 }
