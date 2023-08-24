0be141ca0931b743c4ed26ca8078123b368dddfb
zakariasan <elitziko@gmail.com>
2023-08-24 06:06:45 +0100

    Linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print linked lists
+ * @h: head node
+ *
+ * Description: function that prints all the elements of a list.
+ * Return: nbr of Nodes or NULL
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t Node_nbr;
+
+	Node_nbr = 0;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		Node_nbr++;
+	}
+	return (Node_nbr);
+}
