376dfd23dce2ab9746b349c9bddd49c89eafd29a
zakariasan <elitziko@gmail.com>
2023-08-28 10:00:21 +0100

    Get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return the nth of linked list.
+ * @head: head node
+ * @index: nth to return
+ *
+ * Description: function that deletes the head node of a List.
+ * Return: list of nth data or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp_lst;
+	unsigned int count;
+
+	count = 0;
+	tmp_lst = head;
+	while (tmp_lst)
+	{
+		if (count == index)
+			return (tmp_lst); 
+		tmp_lst = tmp_lst->next;
+		count++;
+	}
+	return (NULL);
+}
