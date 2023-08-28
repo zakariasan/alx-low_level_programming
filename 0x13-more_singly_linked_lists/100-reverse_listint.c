6f21571b6584ab970c10a17667796979127f46bc
zakariasan <elitziko@gmail.com>
2023-08-28 16:27:54 +0100

    revers the list or the word

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse the word list.
+ * @head: head node
+ *
+ * Description:function that reverse a list.
+ * Return: list or NULL
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *lst;
+	listint_t *r_lst;
+
+	lst = *head;
+	r_lst = NULL;
+	while (lst && lst->next)
+	{
+		lst = lst->next;
+		(*head)->next = r_lst;
+		r_lst = *head;
+		*head = lst;
+	}
+	(*head)->next = r_lst;
+	r_lst = *head;
+	return (r_lst);
+}
