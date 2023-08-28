3a9f211bb85dc9b7c9ef9cabf4549bd677299d68
zakariasan <elitziko@gmail.com>
2023-08-28 08:28:21 +0100

    List length

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print linked lists
+ * @h: head node
+ *
+ * Description: function that prints all the elements of a list.
+ * Return: nbr of Nodes or NULL
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t Node_nbr;
+
+	Node_nbr = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		Node_nbr++;
+	}
+	return (Node_nbr);
+}
