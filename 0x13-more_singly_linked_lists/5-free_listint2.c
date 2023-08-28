d6229937e5d8075a59abbc0af37e016a487fd8b4
zakariasan <elitziko@gmail.com>
2023-08-28 17:26:45 +0100

    free the word

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,10 @@ void free_listint2(listint_t **head)
 	listint_t *tmp_lst;
 
 	tmp_lst = *head;
-	while (tmp_lst)
+	while (tmp_lst && head)
 	{
 		tmp_lst = tmp_lst->next;
 		free(*head);
 		*head = tmp_lst;
 	}
-	*head = NULL;
 }
