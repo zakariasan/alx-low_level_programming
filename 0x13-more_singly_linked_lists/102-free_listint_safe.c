cc47cc3dd0c12c6b1c72968e00aae88b1c25dc31
zakariasan <elitziko@gmail.com>
2023-08-29 11:13:26 +0100

    free them a11

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,28 +11,19 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *tmp_lst;
-	listint_t *lst;
 	size_t cnt;
-	size_t i;
 
 	cnt = 0;
 	tmp_lst = *h;
 	while (h && tmp_lst)
 	{
 		tmp_lst = tmp_lst->next;
-		cnt++;
-		i = -1;
-		lst = *h;
-		while (++i < cnt)
+		if (*h - tmp_lst)
 		{
-			if (tmp_lst == lst)
-			{
-				free(lst);
-				return (cnt);
-			}
-			lst = lst->next;
+			*h = tmp_lst;
+			cnt++;
 		}
-		free(tmp_lst);
+		free(*h);
 	}
 	return (cnt);
 }
