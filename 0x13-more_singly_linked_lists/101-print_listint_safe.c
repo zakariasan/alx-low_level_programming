fce1bac8a7aacc1514b937d73c957174f6ce6b90
zakariasan <elitziko@gmail.com>
2023-08-29 10:47:25 +0100

    print listint SAFE

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - Time to print with safe way.
+ * @head: head node
+ *
+ * Description: function that prints a List.
+ * Return: nbr of nodes or 0.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *TMP_lst;
+	const listint_t *LST;
+	size_t cnt;
+	size_t i;
+
+	cnt = 0;
+	TMP_lst = head;
+	while (TMP_lst)
+	{
+		printf("[%p] %d\n", TMP_lst, TMP_lst->n);
+		cnt++;
+		TMP_lst = TMP_lst->next;
+		i = -1;
+		LST = head;
+		while (++i < cnt)
+		{
+			if (TMP_lst == LST)
+			{
+				printf("-> [%p] %d\n", TMP_lst, TMP_lst->n);
+				return (cnt);
+			}
+			LST = LST->next;
+		}
+	}
+	return (cnt);
+}
