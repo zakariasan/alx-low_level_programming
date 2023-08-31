891218a9e590c7f2e270686523fb364a36c5050b
zakariasan <elitziko@gmail.com>
2023-08-31 11:07:00 +0100

    bin unnt

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -31,11 +31,11 @@ unsigned int binary_to_uint(const char *b)
 	nbr = 0;
 	i = -1;
 	j = 0;
-	while (b[++i])
+	while (b[++i] && b)
 		if (b[i] != '0' && b[i] != '1')
 			return (nbr);
 
-	while (--i >= 0 && b[i])
+	while (--i >= 0 && b)
 	{
 		(b[i] == '1') ? (nbr += _pow(j)) : (nbr);
 		j++;
