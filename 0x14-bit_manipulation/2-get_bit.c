f9ac1817e166de467f1dab09f160424e88f14234
zakariasan <elitziko@gmail.com>
2023-08-31 12:18:50 +0100

    get bit 010

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - det the nth bit
+ * @n: nbr.
+ * @index: nth index.
+ *
+ * Description: function that returns the value of a bit at a given index.
+ * Return: the nth value or -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return (n & 1 << index ? 1 : 0);
+}
