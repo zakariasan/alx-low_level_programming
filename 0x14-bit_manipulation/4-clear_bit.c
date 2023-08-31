80ed21230cf4c958ac9fa9653fc83ca138292a42
zakariasan <elitziko@gmail.com>
2023-08-31 12:33:43 +0100

    clear 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear the nth bit
+ * @n: nbr.
+ * @index: nth index.
+ *
+ * Description: function that sets the nth nbr value of nbr.
+ * Return: the 1 value or -1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n = *n & (~1 << index);
+	return (1);
+}
