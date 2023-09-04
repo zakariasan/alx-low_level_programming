f1cd2884a806d15954b04125e1521c660bfcd713
zakariasan <elitziko@gmail.com>
2023-09-04 20:51:07 +0100

    cmgriffing 9

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,9 +35,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (fd);
-	if (write(fd, text_content, _strlen(text_content) <
-				_strlen(text_content)))
-		return (-1);
+	write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
