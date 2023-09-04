f5f5b914ebd4a3e7b60e8fbbde36b80f916eecb2
zakariasan <elitziko@gmail.com>
2023-09-05 00:38:52 +0100

    11 past cpy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,7 +56,7 @@ int main(int ac, char **av)
 		_fprint(STDERR_FILENO, "Error: Can't read from file ", av[1]);
 		exit(98);
 	}
-	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
+	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((size = read(from, bf, 1024)) > 0)
 		if (write(to, bf, size) < 0 || to < 0)
 		{
