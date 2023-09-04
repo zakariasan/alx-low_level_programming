4440038608cbbe1b6287ebcd44a110e17d8b9d2c
zakariasan <elitziko@gmail.com>
2023-09-04 19:44:24 +0100

    adding main0h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+size_t read_textfile(const char *filename, size_t letters);
 #endif /* MAIN_H */
