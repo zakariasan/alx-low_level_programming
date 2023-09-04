1421031c71e0c6a8117df366955cd02d19904a45
zakariasan <elitziko@gmail.com>
2023-09-04 20:52:37 +0100

    Under the snowZ

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (fd);
-	write(fd, text_content, _strlen(text_content));
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
