9e71910f4fd37016b45a6fb0970cf3c9446c0202
zakariasan <elitziko@gmail.com>
2023-10-13 00:32:28 +0100

    add nodeint

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - add new node at the beginning of dlInked list
+ * @head: head.
+ * @n: nbr
+ *
+ * Description: function that adds a new node at the beginning of a dlistint_t.
+ * Return: Null or adress of head
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp;
+
+	tmp = malloc(sizeof(dlistint_t));
+	if (!tmp)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = *head;
+	tmp->prev = NULL;
+	*head = tmp;
+	return (tmp);
+}
