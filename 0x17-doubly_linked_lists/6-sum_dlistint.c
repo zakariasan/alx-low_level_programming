83b00eba387f4ff378dc5f3e116d46360ad9c36c
zakariasan <elitziko@gmail.com>
2023-10-13 02:37:51 +0100

    sum and add item

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - sum of all nth nodes
+ * @head: head.
+ *
+ * Description: function that returns the sum of all nth node of a DlinkedList
+ * Return: result or 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int res;
+
+	res = 0;
+	while (head)
+	{
+		res += head->n;
+		head = head->next;
+	}
+	return (res);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - insert a new node.
+ * @h: head.
+ * @idx: index of node.
+ * @n: nbr.
+ *
+ * Description: function that inserts a new node at a given position.
+ * Return: NULL or node adress
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp = *h;
+	unsigned int i = 0;
+
+	if (idx == i)
+		add_dnodeint(h, n);
+	new = malloc(sizeof(dlistint_t));
+	new->n = n;
+
+	while (tmp)
+	{
+		if (i == idx)
+		{
+			if (!tmp->next)
+				add_dnodeint_end(h, n);
+			else
+			{
+				new->next = tmp;
+				new->prev = tmp->prev;
+				tmp->prev = new;
+				tmp->prev->next = new;
+			}
+			return (new);
+		}
+		i++;
+	}
+	return (NULL);
+}
