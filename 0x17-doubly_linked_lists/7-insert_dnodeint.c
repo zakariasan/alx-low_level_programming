ec488c3e57c3324f6b76e2643525ff51c9e59ff9
zakariasan <elitziko@gmail.com>
2023-10-13 03:24:20 +0100

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx)
 		{
 			if (!tmp->next)
-				add_dnodeint_end(h, n);
+				return (add_dnodeint_end(h, n));
 			else
 			{
 				new->next = tmp;
