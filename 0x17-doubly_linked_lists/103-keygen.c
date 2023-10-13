8859499d4089218af752250166112ea6fb28d8ec
zakariasan <elitziko@gmail.com>
2023-10-13 03:59:48 +0100

    jkey

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+  * main - generates a key, mostly copied from server.
+  * @ac: argument count
+  * @av: argument vectors
+  * Return: 0 on success
+  */
+int main(int ac, char *av[])
+{
+	printf("./crackme5 username key");
+	return (0);
+}
