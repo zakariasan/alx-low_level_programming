8ea413ee63fe1d4f2cf0f75e41f378ecda4e3095
zakariasan <elitziko@gmail.com>
2023-10-17 16:35:05 +0100

    libran22

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
-export LD_PRELOAD=/tmp/libgm.so
+wget -P /tmp https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libran.so
+export LD_PRELOAD=/tmp/libran.so
