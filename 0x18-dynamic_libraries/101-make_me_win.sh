b210939ab6c1a15ac7787c26ae31f399a0c2aaa5
zakariasan <elitziko@gmail.com>
2023-10-17 03:04:45 +0100

    punsh man win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm_lib.so
-export LD_PRELOAD=/tmp/gm_lib.so
+wget -P /tmp/ https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/funlib.so
+export LD_PRELOAD=/tmp/funlib.so
