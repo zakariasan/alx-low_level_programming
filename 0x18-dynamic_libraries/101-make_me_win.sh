2af03d12bd623540ad5b54170057b288d546c4c2
zakariasan <elitziko@gmail.com>
2023-10-17 16:37:09 +0100

    make me

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libran.so
-export LD_PRELOAD=/tmp/libran.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
