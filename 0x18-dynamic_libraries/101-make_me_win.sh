14df0b8ee37f24247e48a52df8cbe17d2900174b
zakariasan <elitziko@gmail.com>
2023-10-17 03:23:34 +0100

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P . https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigafun.so
-export LD_PRELOAD="$PWD/gigafun.so"
+wget -P .. https://github.com/zakariasan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigafun.so
+export LD_PRELOAD="$PWD/../gigafun.so"
