35ed5d1cbb291e2df822effe2baa4e747b2ab950
zakariasan <elitziko@gmail.com>
2023-11-03 13:24:59 +0100

    hash table creat

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
  * hash_table_create - table of hash tables
@@ -14,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash_table || !hash_table->array)
 		return (NULL);
@@ -22,6 +21,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	i = 0;
 	while (i < size)
-		hash_table->array[i++] = NULL;
+	{
+		hash_table->array[i] = NULL;
+		i++;
+	}
 	return (hash_table);
 }
