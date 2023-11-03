2b89c79d84a4d155d87fdf091d75cede7b3be478
zakariasan <elitziko@gmail.com>
2023-11-03 13:47:00 +0100

    key hashig

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,12 +10,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int i;
-	unsigned long int j;
-
-	i = 0;
-	j = i;
-	while (key[i])
-		j += key[i++];
-	return (j % size);
+	return (hash_djb2(key) % size);
 }
