3d1b8e9ad4429822a5d02a0e7a40159c73a0648f
zakariasan <elitziko@gmail.com>
2023-11-04 01:35:53 +0100

    hash set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 /**
@@ -20,13 +21,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	key_tmp = strdup(key);
 	value_tmp = strdup(value);
+	if (!key_tmp || !value_tmp)
+		return (0);
 	idx = key_index((unsigned char *)key_tmp, ht->size);
 	tmp = ht->array[idx];
 	if (!tmp)
 	{
 		tmp = malloc(sizeof(hash_node_t));
 		if (!tmp)
+		{
+			free(key_tmp);
+			free(value_tmp);
 			return (0);
+		}
 		tmp->value = value_tmp;
 		tmp->next = NULL;
 		tmp->key = key_tmp;
@@ -39,16 +46,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(tmp->value);
 			tmp->value = value_tmp;
+			free(key_tmp);
 			return (1);
 		}
 		tmp = tmp->next;
 	}
 	tmp = malloc(sizeof(hash_node_t));
 	if (!tmp)
+	{
+		free(key_tmp);
+		free(value_tmp);
 		return (0);
+	}
 	tmp->value = value_tmp;
 	tmp->key = key_tmp;
 	tmp->next = ht->array[idx];
-	ht->array[idx]->next = tmp;
+	ht->array[idx] = tmp;
 	return (1);
 }
