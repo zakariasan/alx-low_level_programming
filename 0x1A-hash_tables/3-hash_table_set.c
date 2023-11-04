b5c4757bf73ed3bf4a8a1646a0c2c8f1473ec74b
zakariasan <elitziko@gmail.com>
2023-11-04 01:39:08 +0100

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,28 +15,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
 	hash_node_t *tmp;
-	char *key_tmp, *value_tmp;
 
 	if (strlen(key) == 0 || !ht || key == NULL)
 		return (0);
-	key_tmp = strdup(key);
-	value_tmp = strdup(value);
-	if (!key_tmp || !value_tmp)
-		return (0);
-	idx = key_index((unsigned char *)key_tmp, ht->size);
+	idx = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[idx];
 	if (!tmp)
 	{
 		tmp = malloc(sizeof(hash_node_t));
 		if (!tmp)
-		{
-			free(key_tmp);
-			free(value_tmp);
 			return (0);
-		}
-		tmp->value = value_tmp;
+		tmp->value = strdup(value);
 		tmp->next = NULL;
-		tmp->key = key_tmp;
+		tmp->key = strdup(key);
 		ht->array[idx] = tmp;
 		return (1);
 	}
@@ -45,21 +36,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(tmp->key, key) == 0)
 		{
 			free(tmp->value);
-			tmp->value = value_tmp;
-			free(key_tmp);
+			tmp->value = strdup(value);
 			return (1);
 		}
 		tmp = tmp->next;
 	}
 	tmp = malloc(sizeof(hash_node_t));
 	if (!tmp)
-	{
-		free(key_tmp);
-		free(value_tmp);
 		return (0);
-	}
-	tmp->value = value_tmp;
-	tmp->key = key_tmp;
+	tmp->value = strdup(value);
+	tmp->key = strdup(key);
 	tmp->next = ht->array[idx];
 	ht->array[idx] = tmp;
 	return (1);
