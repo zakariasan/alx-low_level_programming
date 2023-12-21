048c7400e77d901783d2ad7964917182d6f4d4fb
zakariasan <elitziko@gmail.com>
2023-12-21 01:05:34 +0100

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,17 +10,19 @@ def island_perimeter(grid):
     a function def island_perimeter(grid): that returns the perimeter of the
     island described in grid:
     """
-    def is_valid(i, j):
-        return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0]) if grid else 0
 
-    edges = 0
-    size = 0
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == 1:
+                if y == 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
+                if y == rows - 1 or grid[y + 1][x] == 0:
+                    perimeter += 1
+                if x == 0 or grid[y][x - 1] == 0:
+                    perimeter += 1
+                if x == cols - 1 or grid[y][x + 1] == 0:
+                    perimeter += 1
 
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
-            if grid[i][j] == 1:
-                size += 1
-                edges += is_valid(i, j - 1) and grid[i][j - 1] == 1
-                edges += is_valid(i - 1, j) and grid[i - 1][j] == 1
-
-    return size * 4 - edges * 2
+    return perimeter
