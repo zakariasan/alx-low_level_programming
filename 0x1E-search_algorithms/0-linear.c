9410b2090dc93a6e6881b32886633a5b97edf5be
zakariasan <elitziko@gmail.com>
2024-02-06 21:01:47 +0100

    search algos

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* linear_search - function that searches for a value in an array.
+* @array: array.
+* @size: size of array. 
+* @value: searchy value 
+* Return: integer
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	
+	while (i < size && array)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}else
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		}
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* ft_print_arr-func to print nbrs 
+* @array: array.
+* @left: start value. 
+* @right: end value. 
+*/
+void ft_print_arr(int *array, int left, int right)
+{
+	int i = left;
+	for (;i <= right; i++)
+	{
+		if (i != right)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+}
+
+/**
+* binary_search - function that searches for a value in an array Binary search.
+* @array: array.
+* @size: size of array. 
+* @value: searchy value 
+* Return: integer
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
+
+	while (left <= right && array)
+	{
+		printf("Searching in array: ");
+		ft_print_arr(array, left, right);
+
+		middle = (left + right) / 2;
+		if (array[middle] < value)
+			left = middle + 1;
+		else if (array[middle] > value)
+			right = middle - 1;
+		else
+			return middle;
+	}
+	return (-1);
+}
