0a2d4f2f05e7efd8281dcbad6f7d853097ae8dc7
zakariasan <elitziko@gmail.com>
2024-02-09 16:55:40 +0100

    interpolation fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,9 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t pos = 0;
-
+	pos = low +
+		(((double)(high - low) / (array[high] - array[low])) *
+		 (value - array[low]));
 	if (array == NULL || size == 0)
 		return (-1);
 
